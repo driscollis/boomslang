30bc222b647b911f2f588dc21ae05ea8a5777a2b
Michael Driscoll <michael@Mikes-MacBook-Pro.local>
2017-05-07 13:18:40 -0500

    updated code to read XML

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import wx
 
+from lxml import etree, objectify
+
+
 class XmlTree(wx.TreeCtrl):
     
     def __init__(self, parent, id, pos, size, style):
         wx.TreeCtrl.__init__(self, parent, id, pos, size, style)
         
+        try:
+            with open(parent.xml_path) as f:
+                xml = f.read()
+        except IOError:
+            print('Bad file')
+            return
+        except Exception as e:
+            print('Really bad error')
+            print(e)
+            return
+        
+        self.xml_root = objectify.fromstring(xml)
+        
+        root = self.AddRoot(self.xml_root.tag)
+        self.SetPyData(root, ('key', 'value'))        
+                
+        for top_level_item in self.xml_root.getchildren():
+            child = self.AppendItem(root, top_level_item.tag)
+            self.SetItemHasChildren(child)
+            if top_level_item.attrib:
+                self.SetPyData(child, top_level_item.attrib)
+        
+        self.Expand(root)
+        self.Bind(wx.EVT_TREE_ITEM_EXPANDING, self.onItemExpanding)
+        
+    def onItemExpanding(self, event):
+        item = event.GetItem()
+        book_id = self.GetPyData(item)
+        
+        for top_level_item in self.xml_root.getchildren():
+            if top_level_item.attrib == book_id:
+                book = top_level_item
+                self.SetPyData(item, top_level_item)                                
+                self.add_book_elements(item, book)
+                break
+            
+    def add_book_elements(self, item, book):
+        for element in book.getchildren():
+            child = self.AppendItem(item, element.tag)
+            if element.getchildren():
+                self.SetItemHasChildren(child)
+        
+            if element.attrib:
+                self.SetPyData(child, element.attrib)  
+                
 
 class TreePanel(wx.Panel):
     
-    def __init__(self, parent):
+    def __init__(self, parent, xml_path):
         wx.Panel.__init__(self, parent)
+        self.xml_path = xml_path
         
         self.tree = XmlTree(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
                             wx.TR_HAS_BUTTONS
                             | wx.TR_EDIT_LABELS)    
-        
-        self.root = self.tree.AddRoot('Something goes here')
-        self.tree.SetPyData(self.root, ('key', 'value'))
-        os = self.tree.AppendItem(self.root, 'Operating Systems')
-        self.tree.Expand(self.root)
-        
+                
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.tree, 0, wx.EXPAND)
         self.SetSizer(sizer)
@@ -27,13 +71,14 @@ class TreePanel(wx.Panel):
         
 class MainFrame(wx.Frame):
     
-    def __init__(self):
+    def __init__(self, xml_path):
         wx.Frame.__init__(self, parent=None, title='XML Editor')
-        panel = TreePanel(self)
+        panel = TreePanel(self, xml_path)
         self.Show()
         
         
 if __name__ == '__main__':
+    xml_path = 'books.xml'
     app = wx.App(redirect=False)
-    frame = MainFrame()
+    frame = MainFrame(xml_path)
     app.MainLoop()
